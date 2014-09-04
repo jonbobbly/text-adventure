d7d73f0c48d7c9e7290a0bea2690e7b977fb5cc4
Jonathon Bly <jonathon@gmail.com>
2014-09-04 01:45:32 -0500

    Working on making People class functional.

--- people.h
+++ people.h
@@ -8,6 +8,8 @@ class People
 	public:
 		People();
 		QString Name;
+		QList<QString> Prompts;		//These show up in list-view widget
+		QList<QString> Responses;	//These show up when list-view item is picked.
 };
 
 #endif // PEOPLE_H
